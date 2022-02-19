c2b671fda7f9a852b0b233851e03525b5b885e8c
Edward Seufert <seufster@gmail.com>
2022-02-19 18:59:22 -0500

    update version

--- toasthub-security/src/main/java/org/toasthub/security/application/ApplicationDaoImpl.java
+++ toasthub-security/src/main/java/org/toasthub/security/application/ApplicationDaoImpl.java
@@ -18,13 +18,12 @@ package org.toasthub.security.application;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
-import org.hibernate.jpa.QueryHints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,39 +47,127 @@ public class ApplicationDaoImpl implements ApplicationDao {
 	@Override
 	public void items(RestRequest request, RestResponse response) throws Exception {
 		
-		String queryStr = "SELECT DISTINCT a FROM Application AS a JOIN FETCH a.title AS t JOIN FETCH t.langTexts as lt ";
+		String queryStr = "SELECT DISTINCT a FROM Application AS a JOIN FETCH a.title AS t JOIN FETCH t.langTexts as lt WHERE lt.lang =:lang ";
 		
-		boolean and = false;
 		if (request.containsParam(GlobalConstant.ACTIVE)) {
-			if (!and) { queryStr += " WHERE "; }
 			queryStr += "a.active =:active ";
-			and = true;
 		}
 		
-		if (request.containsParam(GlobalConstant.SEARCHVALUE) && !request.getParam(GlobalConstant.SEARCHVALUE).equals("")){
-			if (!and) { queryStr += " WHERE "; }
-			queryStr += "lt.lang =:lang AND lt.text LIKE :searchValue"; 
-			and = true;
+		// search
+		ArrayList<LinkedHashMap<String,String>> searchCriteria = null;
+		if (request.containsParam(GlobalConstant.SEARCHCRITERIA) && !request.getParam(GlobalConstant.SEARCHCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.SEARCHCRITERIA) instanceof Map) {
+				searchCriteria = new ArrayList<>();
+				searchCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.SEARCHCRITERIA));
+			} else {
+				searchCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.SEARCHCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean or = false;
+			
+			String lookupStr = "";
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_NAME")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "lt.lang =:lang AND lt.text LIKE :titleValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_CODE")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "a.code LIKE :codeValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_STATUS")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "a.active LIKE :statusValue"; 
+						or = true;
+					}
+				}
+			}
+			if (!"".equals(lookupStr)) {
+				queryStr += " AND ( " + lookupStr + " ) ";
+			}
+			
+		}
+		// order by
+		ArrayList<LinkedHashMap<String,String>> orderCriteria = null;
+		StringBuilder orderItems = new StringBuilder();
+		if (request.containsParam(GlobalConstant.ORDERCRITERIA) && !request.getParam(GlobalConstant.ORDERCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.ORDERCRITERIA) instanceof Map) {
+				orderCriteria = new ArrayList<>();
+				orderCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.ORDERCRITERIA));
+			} else {
+				orderCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.ORDERCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean comma = false;
+			
+			for (LinkedHashMap<String,String> item : orderCriteria) {
+				if (item.containsKey(GlobalConstant.ORDERCOLUMN) && item.containsKey(GlobalConstant.ORDERDIR)) {
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("SYSTEM_APPLICATION_TABLE_NAME")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("lt.text ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("SYSTEM_APPLICATION_TABLE_CODE")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("a.code ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("SYSTEM_APPLICATION_TABLE_STATUS")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("a.active ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+				}
+			}
+		}
+		if (!"".equals(orderItems.toString())) {
+			queryStr += " ORDER BY ".concat(orderItems.toString());
+		} else {
+			// default order
+			queryStr += " ORDER BY lt.text";
 		}
 		
-		TypedQuery<Application> query = entityManagerSecuritySvc.getInstance().createQuery(queryStr, Application.class);
+		Query query = entityManagerSecuritySvc.getInstance().createQuery(queryStr);
+		
+		query.setParameter("lang",request.getParam(GlobalConstant.LANG));
 		
 		if (request.containsParam(GlobalConstant.ACTIVE)) {
 			query.setParameter("active", (Boolean) request.getParam(GlobalConstant.ACTIVE));
 		} 
 		
-		if (request.containsParam(GlobalConstant.SEARCHVALUE) && !request.getParam(GlobalConstant.SEARCHVALUE).equals("")){
-			query.setParameter("searchValue", "%"+((String)request.getParam(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
-			query.setParameter("lang",request.getParam(GlobalConstant.LANG));
+		if (searchCriteria != null){
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {  
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_NAME")){
+						query.setParameter("titleValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_CODE")){
+						query.setParameter("codeValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_STATUS")){
+						if ("active".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", true);
+						} else if ("disabled".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", false);
+						}
+					}
+				}
+			}
 		}
 		if (request.containsParam(GlobalConstant.LISTLIMIT) && (Integer) request.getParam(GlobalConstant.LISTLIMIT) != 0){
 			query.setFirstResult((Integer) request.getParam(GlobalConstant.LISTSTART));
 			query.setMaxResults((Integer) request.getParam(GlobalConstant.LISTLIMIT));
 		}
+		
 
-		List<Application> applications = query.getResultList();
+		List<?> applications = query.getResultList();
 
-		response.addParam(GlobalConstant.ITEMS, applications);
+		response.addParam(GlobalConstant.ITEMS, (List<Application>) applications);
 	}
 
 	@Override
@@ -93,10 +180,46 @@ public class ApplicationDaoImpl implements ApplicationDao {
 			and = true;
 		}
 		
-		if (request.containsParam(GlobalConstant.SEARCHVALUE) && !request.getParam(GlobalConstant.SEARCHVALUE).equals("")){
-			if (!and) { queryStr += " WHERE "; }
-			queryStr += "lt.lang =:lang AND lt.text LIKE :searchValue"; 
-			and = true;
+		ArrayList<LinkedHashMap<String,String>> searchCriteria = null;
+		if (request.containsParam(GlobalConstant.SEARCHCRITERIA) && !request.getParam(GlobalConstant.SEARCHCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.SEARCHCRITERIA) instanceof Map) {
+				searchCriteria = new ArrayList<>();
+				searchCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.SEARCHCRITERIA));
+			} else {
+				searchCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.SEARCHCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean or = false;
+			
+			String lookupStr = "";
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_NAME")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "lt.lang =:lang AND lt.text LIKE :titleValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_CODE")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "m.code LIKE :codeValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_STATUS")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "m.active LIKE :statusValue"; 
+						or = true;
+					}
+				}
+			}
+			if (!"".equals(lookupStr)) {
+				if (!and) { 
+					queryStr += " WHERE ( " + lookupStr + " ) ";
+				} else {
+					queryStr += " AND ( " + lookupStr + " ) ";
+				}
+			}
+			
 		}
 
 		Query query = entityManagerSecuritySvc.getInstance().createQuery(queryStr);
@@ -105,9 +228,25 @@ public class ApplicationDaoImpl implements ApplicationDao {
 			query.setParameter("active", (Boolean) request.getParam(GlobalConstant.ACTIVE));
 		} 
 		
-		if (request.containsParam(GlobalConstant.SEARCHVALUE) && !request.getParam(GlobalConstant.SEARCHVALUE).equals("")){
-			query.setParameter("searchValue", "%"+((String)request.getParam(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
-			query.setParameter("lang",request.getParam(GlobalConstant.LANG));
+		if (searchCriteria != null){
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {  
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_NAME")){
+						query.setParameter("titleValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+						query.setParameter("lang",request.getParam(GlobalConstant.LANG));
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_CODE")){
+						query.setParameter("codeValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("SYSTEM_APPLICATION_TABLE_STATUS")){
+						if ("active".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", true);
+						} else if ("disabled".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", false);
+						}
+					}
+				}
+			}
 		}
 		
 		Long count = (Long) query.getSingleResult();
